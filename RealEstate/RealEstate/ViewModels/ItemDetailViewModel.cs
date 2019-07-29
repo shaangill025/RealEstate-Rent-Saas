9f2a8858a322d88ae674cffd17ff3b5b8daa03f4
Shaanjot Gill <ssgill@gatech.edu>
2019-07-29 06:43:15 -0700

    Initial Commit

---
+++ RealEstate/RealEstate/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RealEstate.Models;
+
+namespace RealEstate.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
